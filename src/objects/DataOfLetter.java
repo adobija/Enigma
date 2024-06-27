b9dccf7e11da8e1d50d8865e5b96f41bf83aeb02
adobija <aledob1324@gmail.com>
2024-06-27 10:53:42 +0200

    Create working reflector

--- src/objects/DataOfLetter.java
+++ src/objects/DataOfLetter.java
@@ -3,4 +3,25 @@ package objects;
 public class DataOfLetter {
    private String letter;
    private boolean shouldRotate;
+
+   public DataOfLetter(String letter, boolean shouldRotate) {
+      this.letter = letter;
+      this.shouldRotate = shouldRotate;
+   }
+
+   public String getLetter() {
+      return letter;
+   }
+
+   public void setLetter(String letter) {
+      this.letter = letter;
+   }
+
+   public boolean isShouldRotate() {
+      return shouldRotate;
+   }
+
+   public void setShouldRotate(boolean shouldRotate) {
+      this.shouldRotate = shouldRotate;
+   }
 }
--- src/objects/Reflector.java
+++ src/objects/Reflector.java
@@ -9,7 +9,7 @@ public class Reflector {
    private List<String> lettersToReflect;
 
     public Reflector(String lettersToReflect) {
-        this.lettersToReflect = List.of(lettersToReflect.split(""));
+        this.lettersToReflect = List.of(lettersToReflect.toUpperCase().split(""));
     }
 
     public DataOfLetter reflectLetter(String letter){
@@ -18,7 +18,9 @@ public class Reflector {
         int i = 0;
         for (String x: lettersToReflect){
             dictionaryOfReflections.put(alphabet[i], x);
+            i++;
         }
-
+        DataOfLetter output = new DataOfLetter(dictionaryOfReflections.get(letter.toUpperCase()), false);
+        return output;
     }
 }
